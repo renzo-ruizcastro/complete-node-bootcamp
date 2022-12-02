e88711cbfb2da1ed934fe009a878d900a61cd0a9
Renzo Ruiz <renzo.ruizcastro@hotmail.com>
2022-12-02 17:28:20 -0500

    feat: mounting routers

--- 4-natours/natours/app.js
+++ 4-natours/natours/app.js
@@ -1,46 +1,49 @@
-// Convention to use app.js for express configuration
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-// middleware
-// app.use(middleware);
-app.use(express.json()); // helps to get the body of the request
+// 1) MIDDLEWARES
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200) // 200 is the default status code
-//     // .send('Hello from the server side!'); // add header content-type: text/html
-//     .json({ message: 'Hello from the server side!', app: 'Natours' }); // add header content-type: application/json
-// });
+app.use(morgan('combined')); // logger middleware
+// morgan doesn't log anything until the response is sent back to the client
 
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
+// middlewares whitout a specified route applies to each request
+// middlewares are secuentially executed
+// bare in mind that they must be inside the request-response cycle
+// .use method is used to mount middlewares
+// express.json() returns a middleware function which is added to the middleware stack
+app.use(express.json());
+// next is a name convention in express
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next(); // allows the request to continue to the next middleware
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const toursPath = `${__dirname}/dev-data/data/tours-simple.json`;
 
 const tours = JSON.parse(fs.readFileSync(toursPath, 'utf-8'));
 
-// Get all tours
-// may require a req body
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
-    // jsend format
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
-    // a succesful put response should return the data specified in the request body
     data: {
       tours,
     },
   });
-});
+};
 
-// Get a single tour
-// optionar parameter (:y?): /api/v1/tours/:id/:x/:y?
-app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params); // { id: <value> }
-  const id = +req.params.id; // or req.params.id * 1
+// 2) ROUTE HANDLERS
+
+const getTour = (req, res) => {
+  const id = +req.params.id;
   const tour = tours.find((el) => el.id === id);
   if (!tour) {
     return res.status(404).json({
@@ -48,22 +51,20 @@ app.get('/api/v1/tours/:id', (req, res) => {
       message: 'Resource not found',
     });
   }
+
   res.status(200).json({
-    // jsend format
     status: 'success',
     data: {
       tour,
     },
   });
-});
+};
 
-// Create a tour
-// obviously requires a req body
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1; // a database would do this for us
-  const newTour = Object.assign({ id: newId }, req.body); // this way you don't mutate the original body object
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
+
   fs.writeFile(toursPath, JSON.stringify(tours), (err) => {
     res.status(201).json({
       status: 'success',
@@ -72,13 +73,10 @@ app.post('/api/v1/tours', (req, res) => {
       },
     });
   });
-  // res.send('Done');
-});
+};
 
-// Update (patch) a tour
-// obviously requires a req body
-app.patch('/api/v1/tours/:id', (req, res) => {
-  const id = +req.params.id; // or req.params.id * 1
+const updateTour = (req, res) => {
+  const id = +req.params.id;
   const tour = tours.find((el) => el.id === id);
   if (!tour) {
     return res.status(404).json({
@@ -86,18 +84,17 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       message: 'Resource not found',
     });
   }
-  
+
   res.status(200).json({
     status: 'success',
     data: {
       tour: '<Updated tour here...>',
-    }
+    },
   });
-});
+};
 
-// Update (patch) a tour
-app.delete('/api/v1/tours/:id', (req, res) => {
-  const id = +req.params.id; // or req.params.id * 1
+const deleteTour = (req, res) => {
+  const id = +req.params.id;
   const tour = tours.find((el) => el.id === id);
   if (!tour) {
     return res.status(404).json({
@@ -105,14 +102,82 @@ app.delete('/api/v1/tours/:id', (req, res) => {
       message: 'Resource not found',
     });
   }
-  
-  // a succesful delete usually sends back a 204 status code
+
   res.status(204).json({
     status: 'success',
-    // a succesful delete response should return null, meaning that the data is deleted
     data: null,
   });
-});
+};
+
+const getAllUsers = (req, res) => {
+  // 500 means internal server error
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+// 3) ROUTES
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+
+// actually http handler methods are middlewares
+// since the route is the same, only the method is different
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+// app.route('/api/v1/users').get(getAllUsers).post(createUser);
+// app
+//   .route('/api/v1/users/:id')
+//   .get(getUser)
+//   .patch(updateUser)
+//   .delete(deleteUser);
+
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+// Do you remember that routes are also middlewares?
+// So, we can mount them as well
+// Here we specify the route for each router/middleware function
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// 4) START SERVER
 
 const port = 3000;
 app.listen(port, () => {
