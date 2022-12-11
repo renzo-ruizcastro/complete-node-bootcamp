d7485bdb3146dc63ad8aceb14e59d97ad0fcedbf
Renzo Ruiz <renzo.ruizcastro@hotmail.com>
2022-12-11 13:46:09 -0500

    feat: refactoring api features

--- 4-natours/natours/controllers/tourController.js
+++ 4-natours/natours/controllers/tourController.js
@@ -16,19 +16,19 @@ const Tour = require('../models/tourModel');
 //   return filteringQuery;
 // };
 
-const formatFilteringQueryOperations = (
-  filteringQueryObj,
-  filteringQueryOperations
-) => {
-  // stringifying the query object to be able to use the replace method to add the $ sign to the operators to match the mongo shell syntax
-  let queryStr = JSON.stringify(filteringQueryObj);
-  const operatorsRegex = new RegExp(
-    `\\b(${filteringQueryOperations.join('|')})\\b`,
-    `g`
-  );
-  queryStr = queryStr.replace(operatorsRegex, (match) => `$${match}`);
-  return JSON.parse(queryStr);
-};
+// const formatFilteringQueryOperations = (
+//   filteringQueryObj,
+//   filteringQueryOperations
+// ) => {
+//   // stringifying the query object to be able to use the replace method to add the $ sign to the operators to match the mongo shell syntax
+//   let queryStr = JSON.stringify(filteringQueryObj);
+//   const operatorsRegex = new RegExp(
+//     `\\b(${filteringQueryOperations.join('|')})\\b`,
+//     `g`
+//   );
+//   queryStr = queryStr.replace(operatorsRegex, (match) => `$${match}`);
+//   return JSON.parse(queryStr);
+// };
 
 // middleware function to alias the top 5 cheap tours
 // after it is called, the getAllTours function is called
@@ -45,23 +45,26 @@ class APIQuery {
   constructor(query, reqQuery) {
     this.query = query;
     this.reqQuery = reqQuery;
+    this.allowedQueryParams = [
+      ['duration', 'difficulty', 'price'], // filtering
+      'sort', // sorting
+      'fields', // field limiting
+      'page', // pagination
+      'limit', // pagination
+    ];
+    this.allowedFilteringQueryOperations = ['gt', 'gte', 'lt', 'lte'];
   }
-
-  static allowedQueryParams = [
-    ['duration', 'difficulty', 'price'], // filtering
-    'sort', // sorting
-    'fields', // field limiting
-    'page', // pagination
-    'limit', // pagination
-  ];
-  static allowedFilteringQueryOperations = ['gt', 'gte', 'lt', 'lte'];
 
   validate() {
     const params = Object.keys(this.reqQuery);
-    const isValid = params.every((param) =>
-      this.allowedQueryParams.flat().includes(param)
-    );
-    return isValid;
+    if (params.length > 0) {
+      const isValid = params.every((param) =>
+        this.allowedQueryParams.flat().includes(param)
+      );
+      if (!isValid) throw new Error('Invalid query parameters');
+      return true;
+    }
+    return false;
   }
 
   find() {
@@ -104,7 +107,7 @@ class APIQuery {
     if (this.reqQuery.limit && !this.reqQuery.page) {
       if (!Number.isInteger(+this.reqQuery.limit) || +this.reqQuery.limit < 1)
         throw new Error('Limit must be an integer greater than 0');
-      this.query = this.query.limit(+this.query.limit);
+      this.query = this.query.limit(+this.reqQuery.limit);
     }
     return this;
   }
@@ -128,138 +131,143 @@ class APIQuery {
 exports.getAllTours = async (req, res) => {
   try {
     console.log('Query:', req.query);
-    const queryObj = { ...req.query };
-    // const allowedQueryParams = [
-    //   ['duration', 'difficulty', 'price'], // filtering
-    //   'sort', // sorting
-    //   'fields', // field limiting
-    //   'page', // pagination
-    //   'limit', // pagination
-    // ];
+    // const queryObj = { ...req.query };
+    // // const allowedQueryParams = [
+    // //   ['duration', 'difficulty', 'price'], // filtering
+    // //   'sort', // sorting
+    // //   'fields', // field limiting
+    // //   'page', // pagination
+    // //   'limit', // pagination
+    // // ];
 
-    let query;
+    // let query;
 
-    // BUILDING QUERY
-    if (Object.keys(queryObj).length > 0) {
-      if (!validateQuery(queryObj, allowedQueryParams.flat()))
-        throw new Error('Invalid query params');
-      // since the queryObj object is similar to the filtering object in mongo shell we will use it to filter
+    // // BUILDING QUERY
+    // if (Object.keys(queryObj).length > 0) {
+    //   if (!validateQuery(queryObj, allowedQueryParams.flat()))
+    //     throw new Error('Invalid query params');
+    //   // since the queryObj object is similar to the filtering object in mongo shell we will use it to filter
 
-      // FILTERING
-      // query includes only fields and values
-      // url query: ?duration=5&difficulty=easy
-      // req query: { duration: '5', difficulty: 'easy' }
-      // mongosh query: .find({ duration: 5, difficulty: 'easy' })
-      // mongoose query: .find({ duration: '5', difficulty: 'easy' })
+    //   // FILTERING
+    //   // query includes only fields and values
+    //   // url query: ?duration=5&difficulty=easy
+    //   // req query: { duration: '5', difficulty: 'easy' }
+    //   // mongosh query: .find({ duration: 5, difficulty: 'easy' })
+    //   // mongoose query: .find({ duration: '5', difficulty: 'easy' })
 
-      // const tours = await Tour.find({ duration: 5, difficulty: 'easy' }); // first way to filter (mongo way)
-      // const tours = await Tour.find()
-      //   .where('duration')
-      //   .equals(5)
-      //   .where('difficulty')
-      //   .equals('easy'); // second way to filter (mongoose way)
+    //   // const tours = await Tour.find({ duration: 5, difficulty: 'easy' }); // first way to filter (mongo way)
+    //   // const tours = await Tour.find()
+    //   //   .where('duration')
+    //   //   .equals(5)
+    //   //   .where('difficulty')
+    //   //   .equals('easy'); // second way to filter (mongoose way)
 
-      // query = Tour.find(); // like in mongo shell, not specifying a filter returns all documents
+    //   // query = Tour.find(); // like in mongo shell, not specifying a filter returns all documents
 
-      // ADVANCED FILTERING
-      // query includes fields, operators and values
-      // url query: ?duration[gte]=5&difficulty=easy
-      // req query: { duration: { gte: '5' }, difficulty: 'easy' }
-      // mongosh query: .find({ duration: { $gte: 5 }, difficulty: 'easy' })
-      // mongoose query: .find({ duration: { $gte: '5' }, difficulty: 'easy' })
+    //   // ADVANCED FILTERING
+    //   // query includes fields, operators and values
+    //   // url query: ?duration[gte]=5&difficulty=easy
+    //   // req query: { duration: { gte: '5' }, difficulty: 'easy' }
+    //   // mongosh query: .find({ duration: { $gte: 5 }, difficulty: 'easy' })
+    //   // mongoose query: .find({ duration: { $gte: '5' }, difficulty: 'easy' })
 
-      const filteringQuery = getFilteringQuery(queryObj, allowedQueryParams[0]);
-      console.log('Filtering query:', filteringQuery);
+    //   const filteringQuery = getFilteringQuery(queryObj, allowedQueryParams[0]);
+    //   console.log('Filtering query:', filteringQuery);
 
-      const allowedFilteringOperators = ['gte', 'gt', 'lte', 'lt'];
-      const formattedFilteringQuery = formatFilteringQueryOperations(
-        filteringQuery,
-        allowedFilteringOperators
-      );
-      console.log('Formatted filtering query:', formattedFilteringQuery);
-      query = Tour.find(formattedFilteringQuery);
+    //   const allowedFilteringOperators = ['gte', 'gt', 'lte', 'lt'];
+    //   const formattedFilteringQuery = formatFilteringQueryOperations(
+    //     filteringQuery,
+    //     allowedFilteringOperators
+    //   );
+    //   console.log('Formatted filtering query:', formattedFilteringQuery);
+    //   query = Tour.find(formattedFilteringQuery);
 
-      // SORTING
-      // sort by fields, by default ascending order
-      // url query: ?sort=price or ?sort=price,ratingsAverage
-      // req query: { sort: 'price' } or  { sort: 'price,ratingsAverage' }
-      // mongosh query: .find().sort({ price: 1 }) or .find().sort({ price: 1, ratingsAverage: 1 })
-      // mongoose query object: .find().sort('price') or .find().sort('price ratingsAverage')
+    //   // SORTING
+    //   // sort by fields, by default ascending order
+    //   // url query: ?sort=price or ?sort=price,ratingsAverage
+    //   // req query: { sort: 'price' } or  { sort: 'price,ratingsAverage' }
+    //   // mongosh query: .find().sort({ price: 1 }) or .find().sort({ price: 1, ratingsAverage: 1 })
+    //   // mongoose query object: .find().sort('price') or .find().sort('price ratingsAverage')
 
-      if (queryObj.sort) {
-        const sortBy = queryObj.sort.split(',').join(' ');
-        query = query.sort(sortBy);
-        // descending way is handled by mongoose, just by adding a - before the sorted field name: ?sort=-price
-        // what if two or more fields have the same value?
-        // you have to specify a second field to sort by: ?sort=price-ratingsAverage
-        // sorting goes from left to right
-      } else {
-        // default sorting
-        // if you're going to sort by a field, this must be returned in the query in which you will sort
-        query = query.sort('-createdAt');
-      }
-      // we remove await here because we may want to add more query methods later
-      // query = Tour.find(queryObj);
+    //   if (queryObj.sort) {
+    //     const sortBy = queryObj.sort.split(',').join(' ');
+    //     query = query.sort(sortBy);
+    //     // descending way is handled by mongoose, just by adding a - before the sorted field name: ?sort=-price
+    //     // what if two or more fields have the same value?
+    //     // you have to specify a second field to sort by: ?sort=price-ratingsAverage
+    //     // sorting goes from left to right
+    //   } else {
+    //     // default sorting
+    //     // if you're going to sort by a field, this must be returned in the query in which you will sort
+    //     query = query.sort('-createdAt');
+    //   }
+    //   // we remove await here because we may want to add more query methods later
+    //   // query = Tour.find(queryObj);
 
-      // FIELD LIMITING
-      // url query: ?fields=name,duration,difficulty,price
-      // req query: { fields: 'name,duration,difficulty,price' }
-      // mongosh query: .find().select({ name: 1, duration: 1, difficulty: 1, price: 1 })
-      // mongoose query object: .find().select('name duration difficulty price')
+    //   // FIELD LIMITING
+    //   // url query: ?fields=name,duration,difficulty,price
+    //   // req query: { fields: 'name,duration,difficulty,price' }
+    //   // mongosh query: .find().select({ name: 1, duration: 1, difficulty: 1, price: 1 })
+    //   // mongoose query object: .find().select('name duration difficulty price')
 
-      if (queryObj.fields) {
-        const fields = queryObj.fields.split(',').join(' ');
-        query = query.select(fields); // projecting: only show the fields specified
-      } else {
-        query = query.select('-__v');
-        // __v is a mongoose field, we don't want to show it
-        // prefixing a field with a - means that we don't want to show it, all other fields will be shown
-      }
+    //   if (queryObj.fields) {
+    //     const fields = queryObj.fields.split(',').join(' ');
+    //     query = query.select(fields); // projecting: only show the fields specified
+    //   } else {
+    //     query = query.select('-__v');
+    //     // __v is a mongoose field, we don't want to show it
+    //     // prefixing a field with a - means that we don't want to show it, all other fields will be shown
+    //   }
 
-      // LIMITING
-      // url query: ?limit=10
-      // req query: { limit: '10' }
-      // mongosh query: .find().limit(10)
-      // mongoose query object: .find().limit(10)
+    //   // LIMITING
+    //   // url query: ?limit=10
+    //   // req query: { limit: '10' }
+    //   // mongosh query: .find().limit(10)
+    //   // mongoose query object: .find().limit(10)
 
-      // avoid problems with pagination
-      if (queryObj.limit && !queryObj.page) {
-        if (!Number.isInteger(+queryObj.limit) || queryObj.limit < 1)
-          throw new Error('Limit must be an integer greater than 0');
-        query = query.limit(+queryObj.limit);
-      }
+    //   // avoid problems with pagination
+    //   if (queryObj.limit && !queryObj.page) {
+    //     if (!Number.isInteger(+queryObj.limit) || queryObj.limit < 1)
+    //       throw new Error('Limit must be an integer greater than 0');
+    //     query = query.limit(+queryObj.limit);
+    //   }
 
-      // PAGINATION
-      // page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page 3...
-      // url query: ?page=2&limit=10
-      // req query: { page: '2', limit: '10' }
-      // mongosh query: .find().skip(10).limit(10)
-      // mongoose query object: .find().skip(10).limit(10)
+    //   // PAGINATION
+    //   // page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page 3...
+    //   // url query: ?page=2&limit=10
+    //   // req query: { page: '2', limit: '10' }
+    //   // mongosh query: .find().skip(10).limit(10)
+    //   // mongoose query object: .find().skip(10).limit(10)
 
-      if (queryObj.page && queryObj.limit) {
-        // must be int
-        if (
-          !Number.isInteger(+queryObj.page) ||
-          !Number.isInteger(+queryObj.limit) ||
-          queryObj.page < 1 ||
-          queryObj.limit < 1
-        )
-          throw new Error('Page and limit must be integers greater than 0');
-        const skip = (queryObj.page - 1) * queryObj.limit;
-        const limit = +queryObj.limit;
-        console.log('skip:', skip, 'limit:', limit);
-        const numTours = await Tour.countDocuments();
-        if (skip >= numTours) throw new Error('This page does not exist');
-        query = query.skip(skip).limit(limit);
-      }
-      // if not specified, we will show all documents
-    } else {
-      query = Tour.find().sort('-createdAt').select('-__v');
-    }
+    //   if (queryObj.page && queryObj.limit) {
+    //     // must be int
+    //     if (
+    //       !Number.isInteger(+queryObj.page) ||
+    //       !Number.isInteger(+queryObj.limit) ||
+    //       queryObj.page < 1 ||
+    //       queryObj.limit < 1
+    //     )
+    //       throw new Error('Page and limit must be integers greater than 0');
+    //     const skip = (queryObj.page - 1) * queryObj.limit;
+    //     const limit = +queryObj.limit;
+    //     console.log('skip:', skip, 'limit:', limit);
+    //     const numTours = await Tour.countDocuments();
+    //     if (skip >= numTours) throw new Error('This page does not exist');
+    //     query = query.skip(skip).limit(limit);
+    //   }
+    //   // if not specified, we will show all documents
+    // } else {
+    //   query = Tour.find().sort('-createdAt').select('-__v');
+    // }
+
+    let query = new APIQuery(Tour.find(), req.query);
+    if (query.validate())
+      query = query.find().sort().select().limit().paginate();
+    else query = query.find().sort('-createdAt').select('-__v');
 
     // EXECUTING QUERY
     // using exec for promises: https://mongoosejs.com/docs/promises.html
-    const tours = await query.exec();
+    const tours = await query.query.exec();
 
     res.status(200).json({
       status: 'success',
@@ -271,7 +279,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err.message,
     });
   }
 };
--- 4-natours/natours/models/tourModel.js
+++ 4-natours/natours/models/tourModel.js
@@ -1,62 +1,65 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    // enum: ['easy', 'medium', 'difficult'], // only these values are allowed
-    required: [true, 'A tour must have a difficulty'],
-  },
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      // enum: ['easy', 'medium', 'difficult'], // only these values are allowed
+      required: [true, 'A tour must have a difficulty'],
+    },
 
-  // not required because is not the user who will specify this
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
+    // not required because is not the user who will specify this
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
 
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'], // [true, error_message]
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'], // [true, error_message]
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true, // removes tailing and leading spaces
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String, // name of the image, this is in the file system
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String], // array of strings
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      // select: false, // this field will not be returned in the response
+    },
+    startDates: [Date],
   },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true, // removes tailing and leading spaces
-    required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String, // name of the image, this is in the file system
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String], // array of strings
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    // select: false, // this field will not be returned in the response
-  },
-  startDates: [Date],
-});
+  { strict: true, strictQuery: 'throw' }
+);
 // mongodb will try to parse each value given to the specified type
 // if it can't, it will throw an error
 
